5ab03bfe3240b67136b61d1240cf5ea7aa2b8b6a
rockitmed162 <127537866+rockitmed162@users.noreply.github.com>
2023-11-24 18:34:49 +0200

    Update Register.aspx.cs

--- Register.aspx.cs
+++ Register.aspx.cs
@@ -10,8 +10,8 @@ namespace WebApplication12
 	{
 		protected void ButtonRegister_Click(object sender, EventArgs e)
 		{
-			string userID = TextBox1.Text;
-			string password = TextBox2.Text;
+			string userID = User.Text;
+			string password = Password.Text;
 
 			// Check if user ID and password have more than 4 characters
 			if (userID.Length > 4 && password.Length > 4)
@@ -24,9 +24,9 @@ namespace WebApplication12
 
 					using (SqlCommand RegisterAccount = new SqlCommand("INSERT INTO Account (UserID, Name, UGradeID, PGradeID, RegDate, Password) VALUES (@UserID, @Name, 0, 0, GETDATE(), @Password)", con))
 					{
-						RegisterAccount.Parameters.AddWithValue("@Name", TextBox1.Text);
-						RegisterAccount.Parameters.AddWithValue("@UserID", TextBox1.Text);
-						RegisterAccount.Parameters.AddWithValue("@Password", TextBox2.Text);
+						RegisterAccount.Parameters.AddWithValue("@Name", User.Text);
+						RegisterAccount.Parameters.AddWithValue("@UserID", User.Text);
+						RegisterAccount.Parameters.AddWithValue("@Password", Password.Text);
 						RegisterAccount.Parameters.AddWithValue("@AID", Session["AID"] ?? "1");
 
 						int rowsAffectedAccount = RegisterAccount.ExecuteNonQuery();
@@ -36,15 +36,15 @@ namespace WebApplication12
 							// Account creation succeeded
 							using (SqlCommand RegisterLogin = new SqlCommand("INSERT INTO Login(UserID, AID, UGradeID, Password) SELECT UserID, AID, UGradeID, Password FROM Account WHERE UserID = @UserID", con))
 							{
-								RegisterLogin.Parameters.AddWithValue("@UserID", TextBox1.Text);
-								RegisterLogin.Parameters.AddWithValue("@Password", TextBox2.Text);
+								RegisterLogin.Parameters.AddWithValue("@UserID", User.Text);
+								RegisterLogin.Parameters.AddWithValue("@Password", Password.Text);
 
 								int rowsAffectedLogin = RegisterLogin.ExecuteNonQuery();
 
 								if (rowsAffectedLogin > 0)
 								{
 									// Login credentials creation succeeded
-									MessageBox.Show(string.Format("Welcome, Your account has been created! Credentials are UserName '{0}' and Password '{1}'", TextBox1.Text, TextBox2.Text));
+									MessageBox.Show(string.Format("Welcome, Your account has been created! Credentials are UserName '{0}' and Password '{1}'", User.Text, Password.Text));
 									Response.Redirect("https://localhost:44396/Pages/Home.aspx");
 								}
 								else
